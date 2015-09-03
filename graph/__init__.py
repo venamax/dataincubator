32924d9db8bd23a44ec97d3ddc7343cd2124eab6
arielist <aritem@gmail.com>
2015-09-03 16:17:43 -0400

    finish graph updates

--- graph/__init__.py
+++ graph/__init__.py
@@ -4,14 +4,14 @@ import typecheck
 @fellow.app.task(name="graph.degree")
 @typecheck.returns("100 * (string, count)")
 def degree():
-    return [('Alec Baldwin', 69)] * 100
+    return [('Alec Baldwin', 82)] * 100
 
 @fellow.app.task(name="graph.pagerank")
 @typecheck.returns("100 * (string, number)")
 def pagerank():
-    return [('Martha Stewart', 0.0002051725372886844)] * 100
+    return [('Martha Stewart', 0.00019312108706213307)] * 100
 
 @fellow.app.task(name="graph.best_friends")
 @typecheck.returns("100 * ((string, string), count)")
 def best_friends():
-    return [(('David Lauren', 'Lauren Bush'), 19)] * 100
+    return [(('Michael Kennedy', 'Eleanora Kennedy'), 41)] * 100
