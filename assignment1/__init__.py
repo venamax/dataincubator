d866f8a8d45027756036ee1f9f3699382ef911c9
Francesco Mosconi <francesco@thedataincubator.com>
2015-12-14 22:56:28 -0500

    removed solution

--- assignment1/__init__.py
+++ assignment1/__init__.py
@@ -5,4 +5,5 @@ import typecheck
 @typecheck.test_cases(x=[1, 2], y=[5, 6.0])
 @typecheck.returns("number")
 def add(x, y):
-    return x + y
+    # change code here to return the sum of x and y
+    return 0
