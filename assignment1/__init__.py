a92f699dc16f15547b6b4ce7390ababa49b654d3
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2016-01-04 12:45:36 -0500

    Adding formal docstring for assignment1

--- assignment1/__init__.py
+++ assignment1/__init__.py
@@ -5,5 +5,5 @@ import typecheck
 @typecheck.test_cases(x=[1, 2], y=[5, 6.0])
 @typecheck.returns("number")
 def add(x, y):
-    # change code here to return the sum of x and y
+    """ change code here to return the sum of x and y """
     return 0
