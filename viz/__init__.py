a3b5ae0829afbbfa3d57eab70c0b1da0240929c4
Christian Moscardi <moscardi79@gmail.com>
2016-02-22 04:05:42 -0500

    viz updated readme and solution file

--- viz/__init__.py
+++ viz/__init__.py
@@ -1,7 +1,22 @@
 import fellow
 import typecheck
 
-#@fellow.app.task(name="spark.upvote_percentage_by_favorites")
-#@typecheck.returns("50 * (count, number)")
-#def upvote_percentage_by_favorites():
-#    return [(20, 0.9952153110047847)] * 50
+@fellow.app.task(name="viz.counts_by_hour")
+@typecheck.returns("string")
+def counts_by_hour():
+    return "http://a-b-c.herokuapp.com"
+
+@fellow.app.task(name="viz.live_count_boxplot")
+@typecheck.returns("string")
+def live_count_boxplot():
+    return "http://a-b-c.herokuapp.com"
+
+@fellow.app.task(name="viz.map_static")
+@typecheck.returns("string")
+def map_static():
+    return "http://a-b-c.herokuapp.com"
+
+@fellow.app.task(name="viz.map_animated")
+@typecheck.returns("string")
+def map_animated():
+    return "http://a-b-c.herokuapp.com"
