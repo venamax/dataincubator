10fc7eede1c6e5675c5ac66c9f77ad41d039f638
Francesco Mosconi <francesco@thedataincubator.com>
2016-03-01 19:23:34 -0500

    grader grades 0 ok

--- music/__init__.py
+++ music/__init__.py
@@ -14,23 +14,25 @@ should score 1%.
 
 import fellow
 import typecheck
+from .data import sample_song_features
 
+keys = ["fe_test_%04d.mp3"%i for i in xrange(1, 146)]
+sol = {k:"blues" for k in keys}
 
 @fellow.app.task(name="music.raw_features_predictions")
-@typecheck.returns("100 * string")
-def food_bigrams():
-    return ["blues"] * 100
-
+@typecheck.returns_dict("string", keys)
+def raw_features_predictions():
+    return sol
 
 
 @fellow.app.task(name="music.all_features_predictions")
-@typecheck.returns("100 * string")
-def food_bigrams():
-    return ["blues"] * 100
+@typecheck.returns_dict("string", keys)
+def all_features_predictions():
+    return sol
 
-@fellow.batch(name="music.raw_features_model")
-@typecheck.test_cases(record=sample_songs)
-@typeheck.returns("number")
-def raw_features_model(record):
-  return 0
+@fellow.batch(name="music.all_features_model")
+@typecheck.test_cases(record=sample_song_features)
+@typecheck.returns("string")
+def all_features_model(record):
+  return "blues"
 
